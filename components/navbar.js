25984dd95e2ce410a548f87c37d976411f4db6a0
Albart Jose <albartbtme@gmail.com>
2021-08-03 21:01:09 +0530

    All In the same files

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+
+    return `<div id="navbar">
+            <div class="StoreName"><h3><a href="recipie.html">Meal Company</a></h3></div>
+            <div class="easyAcesses">
+                <h3><a href="foodSearch.html">Search</a></h3>
+                <h3><a href="recipie.html">Today's Recipie</a></h3>
+                <h3><a href="latRecipie.html">Latest Recipie</a></h3>
+            </div>
+        </div>`
+
+}
+
+
+
+export default navbar
---
+++ scripts/showData.js
@@ -0,0 +1,35 @@
+//fetch('https://fakestoreapi.com/products/category/jewelery')
+
+async function getData(url) {
+    try {
+        let res = await fetch(url)
+        let data = await res.json()
+        return data;
+    }
+    catch (err) {
+        console.log("err:", err);
+    }
+
+
+
+}
+
+function showMeals(meals, parent) {
+    meals.forEach(({ strArea, strCategory, strInstruction, strMeal, strSource, strYoutube, strMealThumb }) => {
+        //console.log(strArea);
+        //console.log(strMealThumb);
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        let name = document.createElement("div");
+        let link = document.createElement("div");
+        img.src = strMealThumb;
+        name.innerHTML = strMeal;
+        link.innerHTML = strSource;
+        div.append(img, name, link);
+        parent.append(div);
+    });
+
+
+}
+
+export { getData, showMeals }
